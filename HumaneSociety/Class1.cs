658d0dfc74959fd8d5b76a24ee1960467cf87ac2
andrew carr <drew4555@yahoo.com>
2019-05-30 11:22:34 -0500

    merging files

---
+++ HumaneSociety/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    class Class1
+    {
+        INSERT INTO Categories VALUES('Dog')
+INSERT INTO Categories VALUES('Cat')
+INSERT INTO Categories VALUES('Dragon')
+INSERT INTO Categories VALUES('Lizard')
+INSERT INTO Categories VALUES('Griffin')
+INSERT INTO DietPlans VALUES('Dog_Food','Blue','2')
+INSERT INTO DietPlans VALUES('Cat_Food','Purina','2')
+INSERT INTO DietPlans VALUES('Dragon_Food','Knights_In_Armour','3000')
+INSERT INTO DietPlans VALUES('Lizard_Food','Crickets','1')
+INSERT INTO DietPlans VALUES('Griffin_Food','Meat','500')
+INSERT INTO Animals VALUES('Spot','16','2','Kind','1','1','Male','Adoptable','1','2','1')
+INSERT INTO Animals VALUES('Perry','1','1','Peaceful','1','1','Male','Adoptable','4','5','1')
+INSERT INTO Animals VALUES('Steve','1600','300','Fiesty','0','0','Male','Not_Adoptable','3','4','1')
+INSERT INTO Animals VALUES('Iba','25','5','Calm','1','0','Female','Adoptable','1','2','1')
+INSERT INTO Animals VALUES('Grif','4','500','Goofy','1','0','Male','Adoptable','5','6','1')
+INSERT INTO Rooms VALUES('100','6')
+INSERT INTO Rooms VALUES('101','8')
+INSERT INTO Rooms VALUES('102','7')
+INSERT INTO Rooms VALUES('103','5')
+INSERT INTO Rooms VALUES('104','4')
+INSERT INTO Employees VALUES('Matt','Washington','NULL','NULL','2','mattwash@ton.com')
+INSERT INTO Employees VALUES('Michael','Heinisch','NULL','NULL','3','Mike@Hein.com')
+INSERT INTO Employees VALUES('Mitchell','Mittelstedt','NULL','NULL','4','mitch@ell.com')
+INSERT INTO Employees VALUES('Michael','Terrill','NULL','NULL','5','michaelterrill@devcodecamp.com')
+INSERT INTO Employees VALUES('Paul','Jirovetz', 'NULL','NULL','6','pauljirovetz@devcodecamp.com')
+INSERT INTO Clients VALUES('Matt','Washington','NULL','NULL','2','mattwash@ton.com')
+INSERT INTO Clients VALUES('Michael','Heinisch','NULL','NULL','3','Mike@Hein.com')
+INSERT INTO Clients VALUES('Mitchell','Mittelstedt','NULL','NULL','4','mitch@ell.com')
+INSERT INTO Clients VALUES('Michael','Terrill','NULL','NULL','5','michaelterrill@devcodecamp.com')
+INSERT INTO Clients VALUES('Paul','Jirovetz', 'NULL','NULL','6','pauljirovetz@devcodecamp.com')
+INSERT INTO Addresses VALUES('123','Milwaukee','49','53204')
+INSERT INTO Addresses VALUES('234','Milwaukee','49','53204')
+INSERT INTO Addresses VALUES('345','Milwaukee','49','53204')
+INSERT INTO Addresses VALUES('456','Milwaukee','49','53204')
+INSERT INTO Addresses VALUES('567','Milwaukee', '49','53204')
+
+    }
+}
